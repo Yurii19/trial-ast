d6caf5f513a2af66779a78211c56fbd436eae65e
Yurii19 <yurikolechkin@gmail.com>
2023-04-15 11:37:09 +0300

    npm: angular material added

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { environment } from './environments/environment';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-bootstrapApplication(AppComponent).catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+    providers: [importProvidersFrom(BrowserAnimationsModule)]
+}).catch(err => console.error(err));
 
 if (environment.production) {
   enableProdMode();
