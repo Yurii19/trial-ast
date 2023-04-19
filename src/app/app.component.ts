189ea0f9baac1b12b9d90812eec21935534377fc
yurii19 <yurikolechkin@gmail.com>
2023-04-19 10:52:02 +0300

    chore: routing added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CardComponent } from './components/card/card.component';
 import { HomeComponent } from './components/home/home.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [CardComponent, HomeComponent],
+  imports: [CardComponent, HomeComponent, RouterModule ],
 })
 export class AppComponent {
   title = 'ast-triapp';
--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,19 @@ import { AppComponent } from './app/app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule, provideHttpClient } from '@angular/common/http';
 //import { HttpClient } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './app/components/home/home.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  //{ path: 'second-component', component: SecondComponent },
+];
 
 bootstrapApplication(AppComponent, {
   providers: [
     importProvidersFrom(BrowserAnimationsModule),
     provideHttpClient(),
+    importProvidersFrom(RouterModule.forRoot(routes)),
   ],
 }).catch((err) => console.error(err));
 
