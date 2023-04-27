25c3d3be2f4284f420edd9b1e78d892cd30dc6e6
yurii19 <yurikolechkin@gmail.com>
2023-04-27 10:28:44 +0300

    chore: added header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CardComponent } from './components/card/card.component';
 import { HomeComponent } from './layouts/home/home.component';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [CardComponent, HomeComponent, RouterModule ],
+  imports: [CardComponent,HeaderComponent, HomeComponent, RouterModule ],
 })
 export class AppComponent {
   title = 'ast-triapp';
